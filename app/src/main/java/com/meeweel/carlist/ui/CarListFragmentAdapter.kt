2c7bad7834fef3cc9f100a93b7eacd53f06b73a6
MeeWeeL <MeeWeeL@yandex.ru>
2022-06-04 14:50:03 +0300

    Glide + Launcher Icon

--- app/src/main/java/com/meeweel/carlist/ui/CarListFragmentAdapter.kt
+++ app/src/main/java/com/meeweel/carlist/ui/CarListFragmentAdapter.kt
@@ -2,9 +2,12 @@ package com.meeweel.carlist.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.meeweel.carlist.R
 import com.meeweel.carlist.databinding.ItemMainRecyclerBinding
 import com.meeweel.carlist.domain.CarModel
 
@@ -38,7 +41,16 @@ class CarListFragmentAdapter :
                     condition.text = it
                 }
                 ("${carItem.cost} RUB").also { cost.text = it }
+                setPicture(this.image, carItem.image)
             }
+        }
+
+        private fun setPicture(imageView: ImageView, imageUrl: String) {
+            Glide.with(imageView.context)
+                .load(imageUrl)
+                .error(R.drawable.ic_no_connection)
+                .placeholder(R.drawable.ic_car)
+                .into(imageView)
         }
     }
 
